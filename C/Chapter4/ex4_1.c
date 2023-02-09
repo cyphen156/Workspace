24fcc741fb418768a5e32aee5a94b4bab7f5f45e
Cyphen <dbdxor@gmail.com>
2023-02-09 15:57:43 +0900

    23.02.09
    study at school with labtop

---
+++ C/Chapter4/ex4_1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int add(int f, int s) {
+	int total;
+	total = f + s;
+	return total;
+}
+
+int main() {
+	int first, second, sum;
+	printf("Enter two input integers.\n");
+	scanf("%d %d", &first, &second);
+	sum = add(first, second);
+	printf("The result is %d.\n", sum);
+	return 0;
+}
