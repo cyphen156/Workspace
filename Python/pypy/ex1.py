82f9c716000d7e34e8df5779515ff18d19dbae64
Cyphen <dbdxor@gmail.com>
2023-04-25 02:03:45 +0900

    sdasda
    asdasd

---
+++ Python/pypy/ex1.py
@@ -0,0 +1,30 @@
+import csv
+import matplotlib.pyplot as plt
+
+fs = open("seoul.csv", encoding="cp949")
+data = csv.reader(fs, delimiter=",")
+header = next(data)
+# print(header)
+maxtmp = 0
+maxdate = ""
+result = []
+day = ""
+for row in data:
+    if row[4] != "":
+        day = row[2]
+        if day.split('-')[1] == '01':
+            tmp = float(row[4])
+            if maxtmp < tmp:
+                maxtmp = tmp
+                maxdate = row[2]
+                print(maxtmp, maxdate)
+
+print("\n{}, {}".format(maxtmp, maxdate))
+## plot = 꺾은선 / hist = 히스토그램 / boxplot = 상자그래프
+# plt.title("hi")
+# plt.plot(result, 'r.', label='result',  linestyle='--', )
+# ## 'r = red' . = circle, ^ = triangle linetyle='dotted'
+# plt.legend() ## label 표시기
+# plt.show()
+# hist(result, bins = 6) ## 가로 축 개수 = 6
+fs.close()
--- Python/week4/ex1.py
+++ Python/week4/ex1.py
@@ -1,12 +1,13 @@
 # file in out
 ## open-read/write-close//overwrite
 file = open(
-    "c:/workspace/Python/week4/read.txt", "r", "utf-8"
+    "c:/workspace/Python/week4/read.txt", mode="r", encoding="utf-8"
 )
-while True:
-    if file != "null":
-        file.writelines(file + "\n")
+# while True:
+#     if file is None:
+#         file.write("file\n")
+
 print(
-    file
+    file.read()
 )
-file.close();
+file.close()
