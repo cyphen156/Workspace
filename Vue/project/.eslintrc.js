447a07a77cc2d41089729f4499438a6b3b9d3513
Cyphen <dbdxor@gmail.com>
2023-09-01 12:52:30 +0900

    23.09.01
    vue 프로젝트 진짜진짜 공부시작

--- Vue/project/.eslintrc.js
+++ Vue/project/.eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-    parserOptions: {
-      parser: 'babel-eslint',
-      requireConfigFile: false,
-    },
-  };
-  
+  parserOptions: {
+    parser: '@babel/eslint-parser', // or 'babel-eslint'
+    requireConfigFile: false,
+  },
+  extends: [
+    // 다른 확장 규칙
+    'plugin:vue/vue3-essential',  // Vue 3에 맞는 ESLint 규칙
+  ],
+};
