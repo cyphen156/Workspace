10287d9abbbc27e9473e198950789a3f8b2b68c0
Cyphen <dbdxor@gmail.com>
2023-09-07 17:03:50 +0900

    dd
    dd

---
+++ C++/AdvancedProgramming2/Week1/Week1/aboutMe.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "학과 : 컴퓨터공학과" << std::endl;
+	std::cout << "학번 : 20170677" << std::endl;
+	std::cout << "이름 : 오융택" << std::endl;
+	//std::cout << "학과 : 컴퓨터공학과\n학번 : 20170677\n이름 : 오융택" << std::endl;
+	return 0;
+}
