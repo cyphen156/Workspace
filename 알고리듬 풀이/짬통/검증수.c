bb7573bf8885c4e4af3bb2f83bcfbb7dfbc8a2fb
Cyphen <dbdxor@gmail.com>
2023-11-15 16:58:17 +0900

    23.11.15
    123

--- 알고리듬 풀이/짬통/검증수.c
+++ 알고리듬 풀이/짬통/검증수.c
@@ -1,45 +1,35 @@
 /**
 * 백준 검증수 2475
-* 
-* 6자리 고유번호 
+*
+* 6자리 고유번호
 * 6번은 검증수, 5개는 시리얼넘버
-* 
+*
 * 검증수  = 시리얼넘버의 각 자리수 **의 합 % 10
-* 
-* 
+*
+*
 * 제한사항
 *****************************************
-* Test = T      						*
-* 1 <= W, H <= 99                       *
-* 1 <= N <= W * H                       *
-* ==> 1 <= N <= 99*99                   *
+* 0 <= N <= 9                           *
 *****************************************
 *
 *
 *
 * 주의
 *
-* 풀이시간 1분
+* 풀이시간 10분
 */
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
 int main() {
-    int T = 0;
-    scanf("%d", &T);
-    for (int i = 1; i <= T; ++i) {
-        // 행번호, 열번호, N번손님
-        int W, H, N, floor = 1, room = 1;
-        scanf("%d%d%d", &H, &W, &N);
-        
-        floor = N % H;
-        room = N / H + 1;
-        if (floor == 0) {
-            floor = H;
-            room--;
-        }
-        printf("%d%02d\n", floor, room);
+    int result = 0;
+    for (int i = 0; i < 5; ++i) {
+        int n = 0;
+        scanf("%d", &n);
+
+        result += n * n;
     }
+    printf("%d", result % 10);
     return 0;
 }
