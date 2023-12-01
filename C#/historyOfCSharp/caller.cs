76f25630de89c09a8ff55ca3838c9eed9c934ad7
Cyphen <dbdxor@gmail.com>
2023-12-01 13:32:53 +0900

    23.12.01
    history of CS(1~6)

---
+++ C#/historyOfCSharp/caller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Logger
+{
+    public static void Log(string message,
+    [CallerMemberName] string memberName = "",
+    [CallerFilePath] string filePath = "",
+    [CallerLineNumber] int lineNumber = 0)
+    {
+        Console.WriteLine($"Message: {message}");
+        Console.WriteLine($"Member Name: {memberName}");
+        Console.WriteLine($"File Path: {filePath}");
+        Console.WriteLine($"Line Number: {lineNumber}");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MyMethod();
+    }
+
+    private static void MyMethod()
+    {
+        Logger.Log("Something happened");
+    }
+}
