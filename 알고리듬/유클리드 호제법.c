701268530fe8909c06e2f91e2de942afba324b73
Cyphen <dbdxor@gmail.com>
2024-02-14 15:10:08 +0900

    24.02.14
    유클리드 호제법 복습

---
+++ 알고리듬/유클리드 호제법.c
@@ -0,0 +1,17 @@
+// 최대공약수
+int GCD(int A, int B) {
+    int C;
+    while(B != 0) {
+        C = A % B;
+        A = B;
+        B = C;
+    }
+    return A;
+}
+
+// 최소공배수
+int LCM(int A, int B, int GCD) {
+    int result;
+    result = A * B / GCD;
+    return result;
+}
