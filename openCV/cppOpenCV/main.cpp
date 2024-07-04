9e57c08169dc51d1b28558c52aa72d8aede8c456
Cyphen <dbdxor@gmail.com>
2024-07-04 18:42:38 +0900

    24.07.04
    내가 뭘 할줄 알까요?

---
+++ openCV/cppOpenCV/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string str = "this is string column";
+    std::cout << "hello World!\n" << str << std::endl;
+    
+    return 0;
+}
