d5f6f21a4f5eab7168cad41ff96554251b45d92e
Cyphen <dbdxor@gmail.com>
2025-02-04 17:47:45 +0900

    25.02.04 BOJ SOLVE 2 Problem
    2563
    10798

--- 알고리듬 풀이/백준/7. 2차원 배열/색종이_2563.cpp
+++ 알고리듬 풀이/백준/7. 2차원 배열/색종이_2563.cpp
@@ -1,10 +1,12 @@
 /**
  * 백준 색종이_2563
- * 
+ * 100 * 100 크기의 정사각형 안에 10 * 10 크기의 종이를 여러장 겹쳐 놓을 때 합 넓이를 구해라
  * 
  * 제한사항
  *****************************************
- * 
+ * 0 < Width, Height <= 100              *
+ * 도화지 밖으로 튀어나가는 색종이는 없다   *
+ * 0 < input Case <= 100                 *
  *****************************************
  *
  *
@@ -12,7 +14,7 @@
  * 주의
  * 
  * 
- * 풀이시간 0분
+ * 풀이시간 10분
  */
 
 
@@ -22,6 +24,30 @@ using namespace std;
 
 int main(void)
 {
-     
+    int input;
+    int plain[100][100] = {0};
+    int cnt = 0;
+
+    cin >> input;
+    for (int i = 0; i < input; ++i)
+    {
+        int x = 0, y = 0;
+        cin >> x >> y;
+        
+        for (int j = x; j < x+10; ++j)
+        {
+            for (int k = y; k < y+10; ++k)
+            {
+                if (plain[j][k] == 1)
+                {
+                    continue;
+                }
+                plain[j][k] = 1;
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt << endl;
     return 0;
 }
--- 알고리듬 풀이/백준/7. 2차원 배열/세로읽기_10798.cpp
+++ 알고리듬 풀이/백준/7. 2차원 배열/세로읽기_10798.cpp
@@ -1,11 +1,13 @@
 /**
  * 백준 세로읽기_10798
- * 
+ * 문자열을 여러개 입력받아 행단위 읽기에서 열단위 읽기로 출력하는 프로그램
  * 
  * 제한사항
- *****************************************
- * 
- *****************************************
+ ************************************************
+ * 0 < strLen <= 15                             *
+ * inputCase = 5                                *
+ * 널문자가 있을 경우 다음 행에 있는 문자를 읽는다. *
+ ************************************************
  *
  *
  *
@@ -17,11 +19,37 @@
 
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(void)
 {
-     
+    // 공백 배열로 초기화
+    char strCage[5][15] = {0};
+
+    for (int i = 0; i < 5; ++i)
+    {
+        string input;
+        getline(cin, input);
+        for (int j = 0; j < input.length(); ++j)
+        {
+            strCage[i][j] = input[j];
+
+        }
+    }
+
+    // 출력
+    for (int i = 0; i < 15; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            if (strCage[j][i] == '\0')
+            {
+                continue;
+            }
+            cout << strCage[j][i];
+        }
+    }
     return 0;
 }
