6244ead8712940588920300e87fe33e171395771
cyphen156 <yungtaekoh@gmail.com>
2025-08-22 14:52:25 +0900

    25.08.22 Algorithms Practice
    2 BOJ Problems Solved
    categories :
    1. Binary Search
    
    2. index array

--- 알고리듬 풀이/백준/25. 이분 탐색/수 찾기_1920.cpp
+++ 알고리듬 풀이/백준/25. 이분 탐색/수 찾기_1920.cpp
@@ -1,10 +1,13 @@
 /**
  * 백준 수 찾기_1920
- * 
+ * N개의 정수중 X라는 정수가 있는지 빠른 시간 안에 찾아내라.
+ * 시간복잡도가 O(N)이어선 안된다.
+ * 출력 : true == 1, false == 0
  * 
  * 제한사항
  *****************************************
- * 
+ * 1 <= N, M < 100001                    *
+ * Input is Intiger                      *
  *****************************************
  *
  *
@@ -13,13 +16,20 @@
  * 없다.
  * 
  * 풀이시간 (문제 해석 + 구현)
- * 0 + 0분
+ * 1 + 10분
  */
 
 
 #include <iostream>
+#include <algorithm>
 
+static const int MAX_SIZE = 100000;
 using namespace std;
+
+static int N, M;
+static int inputs[MAX_SIZE] = { 0 };
+
+bool BinarySearch(int value);
 
 int main(void)
 {
@@ -27,5 +37,49 @@ int main(void)
     cin.tie(NULL);
     cout.tie(NULL);
     
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> inputs[i];
+    }
+
+    cin >> M;
+
+    sort(inputs, inputs + N);
+
+    for (int i = 0; i < M; ++i)
+    {
+        int input;
+        cin >> input;
+        cout << BinarySearch(input) << '\n';
+    }
+    return 0;
+}
+
+bool BinarySearch(int value)
+{
+    int left = 0;
+    int right = N - 1;
+    int pivot;
+
+    while (left <= right)
+    {
+        pivot = (left + right) / 2;
+
+        if (inputs[pivot] == value)
+        {
+            return 1;
+        }
+
+        if (inputs[pivot] < value)
+        {
+            left = pivot + 1;
+        }
+        else 
+        {
+            right = pivot - 1;
+        }
+    }
     return 0;
 }
--- 알고리듬 풀이/백준/25. 이분 탐색/숫자 카드 2_10816.cpp
+++ 알고리듬 풀이/백준/25. 이분 탐색/숫자 카드 2_10816.cpp
@@ -1,10 +1,12 @@
 /**
  * 백준 숫자 카드 2_10816
- * 
+ * N개의 정수 배열이 있다.
+ * 이중 M개의 정수를 입력받았을 때 존재하는 숫자 개수를 구하라. 
  * 
  * 제한사항
  *****************************************
- * 
+ * 1 <= N, M < 500,001                   *
+ * -10,000,000 <= value < 10,000,001     *
  *****************************************
  *
  *
@@ -13,7 +15,7 @@
  * 없다.
  * 
  * 풀이시간 (문제 해석 + 구현)
- * 0 + 0분
+ * 1 + 5분
  */
 
 
@@ -21,11 +23,35 @@
 
 using namespace std;
 
+static const int MAX_SIZE   = 20000001;
+static const int OFFSET     = 10000000;
+
+static int N, M;
+static int inputs[MAX_SIZE] = { 0 };
+
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        int input;
+        cin >> input;
+        inputs[input + OFFSET]++;
+    }
+
+    cin >> M;
+
+    for (int i = 0; i < M; ++i)
+    {
+        int input;
+        cin >> input;
+        cout << inputs[input + OFFSET] << ' ';
+    }
+    cout << endl;
     return 0;
 }
