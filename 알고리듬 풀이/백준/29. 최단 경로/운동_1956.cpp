956b6162c7e800f23f03ee7d4eb5dd62a29485dd
cyphen156 <yungtaekoh@gmail.com>
2025-09-11 14:26:42 +0900

    25.09.11 Algorithms Practice
    2 BOJ Problems Solved
    categories :
    1. Shortest Path

--- 알고리듬 풀이/백준/29. 최단 경로/운동_1956.cpp
+++ 알고리듬 풀이/백준/29. 최단 경로/운동_1956.cpp
@@ -1,10 +1,15 @@
 /**
  * 백준 운동_1956
- * 
+ * V개의 마을과 E개의 도로
+ * 방향 그래프, 목표에 갓다가 다시 시작점으로 돌아와야 한다.
+ * 길이 합이 최소가 되도록해야한다.
  * 
  * 제한사항
  *****************************************
- * 
+ * 2 <= V < 401                          *
+ * 0 <= E < V(V - 1) + 1                 *
+ * a is Start, b is End, c is price      *
+ * 1 <= C < 10,001                       *
  *****************************************
  *
  *
@@ -13,19 +18,96 @@
  * 없다.
  * 
  * 풀이시간 (문제 해석 + 구현)
- * 0 + 0분
+ * 3 + 20분
  */
 
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+const int INF = 0x7fffffff;
+
+static int V, E;
+
+vector<vector<int>> dist;
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    cin >> V >> E;
+
+    dist.assign(V + 1, vector<int>(V + 1, INF));
+
+    for (int i = 0; i < E; ++i)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (c < dist[a][b])
+        {
+            dist[a][b] = c;
+        }    
+    }
     
+    for (int i = 0; i < V + 1; i++)
+    {
+        for (int j = 0; j < V + 1; j++)
+        {
+            if (dist[j][i] == INF)
+            {
+                continue;
+            }
+            for (int k = 0; k < V + 1; k++)
+            {
+                if (dist[i][k] == INF)
+                {
+                    continue;
+                }
+                int candidate = dist[j][i] + dist[i][k];
+                if (candidate < dist[j][k])
+                {
+                    dist[j][k] = candidate;
+                }
+            }
+        }
+    }
+
+    int result = INF;
+
+    for (int i = 1; i <= V; ++i)
+    {
+        for (int j = 1; j <= V; ++j)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            // 경로 없음
+            if (dist[i][j] == INF || dist[j][i] == INF)
+            {
+                continue;
+            }
+            int cycle = dist[i][j] + dist[j][i];
+            if (cycle < result)
+            {
+                result = cycle;
+            }
+        }
+    }
+
+    if (result == INF)
+    {
+        cout << -1 << '\n';
+    }
+    else
+    {
+        cout << result << '\n';
+    }
     return 0;
 }
--- 알고리듬 풀이/백준/29. 최단 경로/플로이드_11404.cpp
+++ 알고리듬 풀이/백준/29. 최단 경로/플로이드_11404.cpp
@@ -1,10 +1,23 @@
 /**
  * 백준 플로이드_11404
+ * N개의 도시와 M개의 경로가 있다.
+ * 각 경로에는 필요 비용이 존재한다.
+ * 모든 쌍에 대해서 A에서 B로 가는데 필요한 비용의 최솟값을 구하라.
+ * 
+ * 
+ * 출력
+ * n개의 줄을 출력해야 한다.
+ * i번째 줄에 출력하는 j번째 숫자는 도시 i에서 j로 가는데 필요한 최소 비용이다.
+ * 만약, i에서 j로 갈 수 없는 경우에는 그 자리에 0을 출력한다.
  * 
  * 
  * 제한사항
  *****************************************
- * 
+ * 1<= N < 101                           *
+ * 1 <= M < 100,001                      *
+ * a is Start, b is End, c is Price      *
+ * a != b                                *
+ * 1 <= c < 100,001                      *
  *****************************************
  *
  *
@@ -13,13 +26,21 @@
  * 없다.
  * 
  * 풀이시간 (문제 해석 + 구현)
- * 0 + 0분
+ * 3 + 20분
  */
 
 
 #include <iostream>
+#include <vector>
+#include <utility>
 
 using namespace std;
+
+const int INF = 0x7fffffff;
+
+static int N, M;
+
+vector<vector<int>> dist;
 
 int main(void)
 {
@@ -27,5 +48,70 @@ int main(void)
     cin.tie(NULL);
     cout.tie(NULL);
     
+    cin >> N;
+    cin >> M;
+
+    dist.assign(N + 1, vector<int>(N + 1, INF));
+
+    // 자기 자신 처리
+    for (int i = 1; i <= N; ++i)
+    {
+        dist[i][i] = 0;
+    }
+    
+    for (int i = 0; i < M; ++i)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        // 최소 거리만 저장하고 나머지 제외 
+        // 동일 출발 - 목적지 경로 압축
+        if (c < dist[a][b])
+        {
+            dist[a][b] = c;
+        }
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int j = 1; j <= N; ++j)
+        {
+            if (dist[j][i] == INF)
+            {
+                continue;
+            }
+            for (int k = 1; k <= N; ++k)
+            {
+                if (dist[i][k] == INF)
+                {
+                    continue;
+                }
+                int candidate = dist[j][i] + dist[i][k];
+                
+                if (candidate < dist[j][k])
+                {
+                    dist[j][k] = candidate;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int j = 1; j <= N; ++j)
+        {
+            if (dist[i][j] == INF)
+            {
+                cout << 0;
+            }
+            else
+            {
+                cout << dist[i][j];    
+            }
+            cout << ' ';
+        }
+        cout << '\n';
+    }
+
     return 0;
 }
