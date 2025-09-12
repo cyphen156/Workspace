06453d3ad6485cc38272cd592738f1c0ac3e9798
cyphen156 <yungtaekoh@gmail.com>
2025-09-12 19:09:35 +0900

    25.09.12 Algorithms Practice
    1 BOJ Problems Solved
    categories : Two Pointers

--- 알고리듬 풀이/백준/30. 투 포인터/두 수의 합_3273.cpp
+++ 알고리듬 풀이/백준/30. 투 포인터/두 수의 합_3273.cpp
@@ -1,10 +1,11 @@
 /**
  * 백준 두 수의 합_3273
- * 
+ * ai + aj = x인 두 수의 쌍을 모두 구하라.
  * 
  * 제한사항
  *****************************************
- * 
+ * 1 <= n < 100,001                      *
+ * 1 <= x < 2,000,001                    *
  *****************************************
  *
  *
@@ -13,13 +14,22 @@
  * 없다.
  * 
  * 풀이시간 (문제 해석 + 구현)
- * 0 + 0분
+ * 1 + 5분
  */
 
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+static const int MAX_SIZE = 100001;
+
+static int n, x, result;
+
+static int numbers[MAX_SIZE] = { 0 };
+
+void solve(int x);
 
 int main(void)
 {
@@ -27,5 +37,45 @@ int main(void)
     cin.tie(NULL);
     cout.tie(NULL);
     
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> numbers[i];
+    }
+
+    sort(numbers, numbers + n);
+    
+    cin >> x;
+    
+    solve(x);
+
+    cout << result << '\n';
     return 0;
+}
+
+void solve(int x)
+{
+    int left = 0;
+    int right = n-1;
+
+    while(left < right)
+    {
+        int sum = numbers[left] + numbers[right];
+
+        if (sum == x)
+        {
+            ++result;
+            ++left;
+            --right;
+        }
+        else if (sum < x)
+        {
+            ++left;
+        }
+        else
+        {
+            --right;
+        }
+    }
 }
